19eb4786b421f87525f13c1d1fba5640c710487a
yyama <dummy@ne.jp>
2019-06-09 12:57:03 +0900

    パラメータが受け取れなかった不具合修正

--- src/main/java/org/yyama/twittorej/api/HttpAPI.java
+++ src/main/java/org/yyama/twittorej/api/HttpAPI.java
@@ -16,8 +16,8 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -28,8 +28,8 @@ import com.fasterxml.jackson.databind.SerializationFeature;
 public class HttpAPI {
 
 	@ResponseBody
-	@RequestMapping("/getOg")
-	public ResponseEntity<String> getOg(@PathVariable("url") String url) {
+	@RequestMapping("/getog")
+	public ResponseEntity<String> getOg(@RequestParam("url") String url) {
 
 		HttpHeaders headers = new HttpHeaders();
 		headers.add("Content-Type:", "application/json");
